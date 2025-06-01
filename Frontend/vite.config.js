84149e7861d2382d69897fa3baa2ec903bd5458e
Ayankhan086 <kmayankhan1@gmail.com>
2025-06-01 15:17:21 +0500

    fix: Update '@assets' alias path to point to the public directory

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, './src/assets')
+      '@assets': path.resolve(__dirname, './public')
     }
   }
 })
