c2251c6599f97a49bf4d34926b9ca81f92761043
Ayankhan086 <kmayankhan1@gmail.com>
2025-07-18 10:15:19 +0500

    fix: update server URL in .env and adjust CORS configuration for production

--- Server/src/app.js
+++ Server/src/app.js
@@ -5,25 +5,23 @@ import cookieParser from "cookie-parser"
 import { Server } from "socket.io"
 
 const app = express()
-
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
-    credentials: true
-  }
-))
-
 const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://social-me-n9nv.vercel.app",
     credentials: true,
     allowedHeaders: ["cookie", "authorization"]
   },
   path: "/api/v1/socket.io"
 })
 
+app.use(cors(
+  {
+    origin: "https://social-me-n9nv.vercel.app",
+    credentials: true
+  }
+))
 
 
 app.use(express.json({ limit: "16kb" }))
