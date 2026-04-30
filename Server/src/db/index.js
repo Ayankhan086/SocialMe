03e88f155e2feb28379d4c74e3f4ce5459a0d388
Ayankhan086 <kmayankhan1@gmail.com>
2026-04-30 17:09:40 +0500

    feat: implement MongoDB connection logic and define database name constant

--- Server/src/db/index.js
+++ Server/src/db/index.js
@@ -1,12 +1,11 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-
 const connectDB = async () => {
     try {
         const uri = process.env.MONGODB_URI ? process.env.MONGODB_URI.trim() : "";
         const connectionInstance = await mongoose.connect(uri, {
-            dbName: DB_NAME
+            dbName: DB_NAME // This ensures you use "SocialMe" as the database
         })
         console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
     } catch (error) { 
